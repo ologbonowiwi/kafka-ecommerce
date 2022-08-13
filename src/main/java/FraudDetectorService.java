ba50871f3c5ced71215113699c49a99dbd0455e2
Wesley Matos <wm4tos.777@gmail.com>
2022-08-13 14:51:19 -0300

    feat: implement new order consumer

---
+++ src/main/java/FraudDetectorService.java
@@ -0,0 +1,53 @@
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class FraudDetectorService {
+    public static void main(String[] args) {
+        Consumer<String, String> consumer = new KafkaConsumer<>(properties());
+
+        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            if (!records.isEmpty()) {
+                System.out.printf("%d registers found\n", records.count());
+
+                for (var record : records) {
+                    System.out.println("-------------------------------");
+                    System.out.println("Checking for fraud on new order");
+                    System.out.printf("Key: %s\n", record.key());
+                    System.out.printf("Value: %s\n", record.value());
+                    System.out.printf("Partition: %d\n", record.partition());
+                    System.out.printf("Offset: %d%n\n", record.offset());
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        // ignoring
+                        e.printStackTrace();
+                    }
+                    System.out.println("Order processed");
+                }
+            }
+        }
+    }
+
+    private static @NotNull Properties properties() {
+        Properties properties = new Properties();
+
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getName());
+
+        return properties;
+    }
+}
