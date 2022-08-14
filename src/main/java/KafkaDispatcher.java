68973fe8b720e3711300a1a1cd35db528f9a6c23
Wesley Matos <wm4tos.777@gmail.com>
2022-08-13 21:35:22 -0300

    refactor: using a kafka dispatcher to avoid code duplication on producers

---
+++ src/main/java/KafkaDispatcher.java
@@ -0,0 +1,46 @@
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class KafkaDispatcher {
+    private final KafkaProducer<String, String> producer = new KafkaProducer<>(properties());
+    private static final String BOOTSTRAP_SERVER = "127.0.0.1:9092";
+
+    public void send(String topic, String key, String value) {
+        ProducerRecord<String, String> message = new ProducerRecord<>(topic, key, value);
+
+        try {
+            this.producer.send(message, getCallback()).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @NotNull
+    private static Callback getCallback() {
+        return (data, ex) -> {
+            if (ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+
+            System.out.printf("Message sent successfully %s | partition %d | offset | %d%n | time %d%n", data.topic(), data.partition(), data.offset(), data.timestamp());
+        };
+    }
+
+    private static @NotNull Properties properties() {
+        Properties properties = new Properties();
+
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return properties;
+    }
+}
--- src/main/java/NewOrderMain.java
+++ src/main/java/NewOrderMain.java
@@ -1,50 +1,18 @@
-import org.apache.kafka.clients.producer.*;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Properties;
 import java.util.UUID;
-import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        Producer<String, String> producer = new KafkaProducer<>(properties());
+    public static void main(String[] args) {
+        KafkaDispatcher dispatcher = new KafkaDispatcher();
 
         for (int i = 0; i < 100; i++) {
             String key = UUID.randomUUID().toString();
 
             String newOrderValue = "123214521,123125412321,21451253123"; // id_request, id_order, valor
-            ProducerRecord<String, String> newOrderRecord = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, newOrderValue);
-
-            producer.send(newOrderRecord, getCallback()).get();
+            dispatcher.send("ECOMMERCE_NEW_ORDER", key, newOrderValue);
 
             String emailValue = "Thank you for your order! We are processing your order!";
-            ProducerRecord<String, String> emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, emailValue);
-
-            producer.send(emailRecord, getCallback()).get();
+            dispatcher.send("ECOMMERCE_SEND_EMAIL", key, emailValue);
         }
-    }
-
-    @NotNull
-    private static Callback getCallback() {
-        return (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
-            }
-
-            System.out.printf("Message sent successfully %s | partition %d | offset | %d%n | time %d%n", data.topic(), data.partition(), data.offset(), data.timestamp());
-        };
-    }
-
-    private static @NotNull Properties properties() {
-        Properties properties = new Properties();
-
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
-        return properties;
     }
 }
