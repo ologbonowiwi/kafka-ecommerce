9938af3974f50a78984e0f603cc6840d84719249
Wesley Matos <wm4tos.777@gmail.com>
2022-08-13 22:13:06 -0300

    refactor: create dispatch for email and for order independently

--- src/main/java/ninja/wmatos/ecommerce/NewOrderMain.java
+++ src/main/java/ninja/wmatos/ecommerce/NewOrderMain.java
@@ -1,19 +1,24 @@
 package ninja.wmatos.ecommerce;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public class NewOrderMain {
 
     public static void main(String[] args) {
-        try (KafkaDispatcher dispatcher = new KafkaDispatcher()) {
+        try (KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>(); KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>()) {
             for (int i = 0; i < 100; i++) {
                 String key = UUID.randomUUID().toString();
 
-                String newOrderValue = "123214521,123125412321,21451253123"; // id_request, id_order, valor
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, newOrderValue);
+                String userId = UUID.randomUUID().toString();
+                String orderId = UUID.randomUUID().toString();
+                BigDecimal amount = BigDecimal.valueOf(Math.random() * 5000 + 1);
+                Order newOrder = new Order(userId, orderId, amount);
+
+                orderDispatcher.send("ECOMMERCE_NEW_ORDER", key, newOrder);
 
                 String emailValue = "Thank you for your order! We are processing your order!";
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, emailValue);
+                emailDispatcher.send("ECOMMERCE_SEND_EMAIL", key, emailValue);
             }
         }
     }
