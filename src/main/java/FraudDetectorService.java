04cc76c20be06a64cbc1743b07b1dffa3c21f5a8
Wesley Matos <wm4tos.777@gmail.com>
2022-08-13 21:16:53 -0300

    refactor: use kafka service to reduce code duplication

--- src/main/java/FraudDetectorService.java
+++ src/main/java/FraudDetectorService.java
@@ -1,56 +1,26 @@
-import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.jetbrains.annotations.NotNull;
-
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
-import java.util.UUID;
+import org.apache.kafka.clients.consumer.*;
 
 public class FraudDetectorService {
-    public static void main(String[] args) {
-        Consumer<String, String> consumer = new KafkaConsumer<>(properties());
-
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
-
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-
-            if (!records.isEmpty()) {
-                System.out.printf("%d registers found\n", records.count());
-
-                for (var record : records) {
-                    System.out.println("-------------------------------");
-                    System.out.println("Checking for fraud on new order");
-                    System.out.printf("Key: %s\n", record.key());
-                    System.out.printf("Value: %s\n", record.value());
-                    System.out.printf("Partition: %d\n", record.partition());
-                    System.out.printf("Offset: %d%n\n", record.offset());
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        // ignoring
-                        e.printStackTrace();
-                    }
-                    System.out.println("Order processed");
-                }
-            }
+    private void runner(ConsumerRecord<String, String> record) {
+        System.out.println("-------------------------------");
+        System.out.println("Checking for fraud on new order");
+        System.out.printf("Key: %s\n", record.key());
+        System.out.printf("Value: %s\n", record.value());
+        System.out.printf("Partition: %d\n", record.partition());
+        System.out.printf("Offset: %d%n\n", record.offset());
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            // ignoring
+            e.printStackTrace();
         }
+        System.out.println("Order processed");
     }
 
-    private static @NotNull Properties properties() {
-        Properties properties = new Properties();
+    public static void main(String[] args) {
+        FraudDetectorService fraudDetector = new FraudDetectorService();
+        KafkaService service = new KafkaService(FraudDetectorService.class.getName(), "ECOMMERCE_NEW_ORDER", fraudDetector::runner);
 
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getName());
-        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getName() + "-" + UUID.randomUUID().toString());
-        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
-
-        return properties;
+        service.run();
     }
 }
